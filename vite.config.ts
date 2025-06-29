4b297eee1120bc4067e33252941b94353f79d6ce
Kenta Mukai <3153823+muk-ai@users.noreply.github.com>
2025-06-29 17:05:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/work-estimation-tool/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/pdf-estimating-v2/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
